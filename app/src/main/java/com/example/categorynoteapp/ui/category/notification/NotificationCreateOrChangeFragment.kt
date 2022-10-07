b3d9aff06dfb10937f3e65efd0e2feee9c9c31dd
polinas888 <p.shcherbinina@itransition.com>
2022-10-07 15:46:09 +0300

    add notification create or change fragment

---
+++ app/src/main/java/com/example/categorynoteapp/ui/category/notification/NotificationCreateOrChangeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.categorynoteapp.ui.category.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.categorynoteapp.R
+
+class NotificationCreateOrChangeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.notification_create_or_change_fragment, container, false)
+    }
+}
