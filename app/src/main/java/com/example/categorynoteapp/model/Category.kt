f21f82d724dec3b65bc92a5e8019b9948e992ffa
polinas888 <p.shcherbinina@itransition.com>
2022-10-07 15:24:25 +0300

    added model

---
+++ app/src/main/java/com/example/categorynoteapp/model/Category.kt
@@ -0,0 +1,6 @@
+package com.example.categorynoteapp.model
+
+data class Category(
+    val id: String,
+    var name: String
+)
---
+++ app/src/main/java/com/example/categorynoteapp/model/Notification.kt
@@ -0,0 +1,6 @@
+package com.example.categorynoteapp.model
+
+data class Notification(
+    val id: Int = 0,
+    var text: String
+)
