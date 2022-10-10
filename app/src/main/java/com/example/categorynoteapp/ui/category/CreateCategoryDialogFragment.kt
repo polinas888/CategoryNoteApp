323d95be52dfd12f4222603274ab8cb437fc0061
polinas888 <p.shcherbinina@itransition.com>
2022-10-10 10:38:47 +0300

    single responsibility principle for CreateCategoryDialogFragment

--- app/src/main/java/com/example/categorynoteapp/ui/category/CreateCategoryDialogFragment.kt
+++ app/src/main/java/com/example/categorynoteapp/ui/category/CreateCategoryDialogFragment.kt
@@ -12,10 +12,7 @@ import com.example.categorynoteapp.databinding.FragmentCreateCategoryDialogBindi
 class CreateCategoryDialogFragment: DialogFragment() {
     private lateinit var binding: FragmentCreateCategoryDialogBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentCreateCategoryDialogBinding.inflate(layoutInflater)
         binding.cancelButton.setOnClickListener {
@@ -23,14 +20,18 @@ class CreateCategoryDialogFragment: DialogFragment() {
         }
 
         binding.okButton.setOnClickListener {
-            val category = binding.createCategoryEditText.text.toString()
-            if(category.isNotEmpty()) {
-                val i: Intent = Intent()
-                    .putExtra("category", category)
-                targetFragment!!.onActivityResult(targetRequestCode, Activity.RESULT_OK, i)
-            }
+            // Single Responsibility Principle, created separate method runOnActivityResultAndPassCategoryArg
+            runOnActivityResultAndPassCategoryArg()
             dismiss()
         }
         return binding.root
+    }
+
+    private fun runOnActivityResultAndPassCategoryArg() {
+        val category = binding.createCategoryEditText.text.toString()
+        if(category.isNotEmpty()) {
+            val intent: Intent = Intent().putExtra("category", category)
+            targetFragment!!.onActivityResult(targetRequestCode, Activity.RESULT_OK, intent)
+        }
     }
 }
