b0c25542bb86f636e93b1045ce5e58a15b6f3d1a
polinas888 <p.shcherbinina@itransition.com>
2022-11-02 10:39:54 +0300

    extension function addFragment for activity

--- app/src/main/java/com/example/categorynoteapp/Extentions.kt
+++ app/src/main/java/com/example/categorynoteapp/Extentions.kt
@@ -1,10 +1,13 @@
 package com.example.categorynoteapp
 
+import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
+import com.example.categorynoteapp.ui.category.CategoryFragment
 
 fun Fragment.changeFragment(args: Bundle, fragmentManager: FragmentManager) {
     this.arguments = args
@@ -13,6 +16,12 @@ fun Fragment.changeFragment(args: Bundle, fragmentManager: FragmentManager) {
         addToBackStack(null)
         replace(R.id.container, this@changeFragment)
     }
+}
+
+fun AppCompatActivity.addFragment(
+    newFragment: Fragment,
+    containerID : Int = R.id.container) {
+    supportFragmentManager.beginTransaction().add(containerID, newFragment).commit()
 }
 
 /* Single Responsibility Principle method close fragment
